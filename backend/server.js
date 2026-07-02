5488748a7dc9bd36014aad82915d78e89e330728
GAMT200 <mt2020.va@gmail.com>
2026-07-01 23:43:28 -0500

    connected dataBase

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Protestudio backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+const db = require("./config/db");
+
+app.get("/test-db", async(req, res) => {
+    try{
+        const [rows] = await db.query("SELECT * FROM clients");
+        res.json(rows);
+    } catch(err) {
+        res.status(500).json({error: err.message});
+    }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
